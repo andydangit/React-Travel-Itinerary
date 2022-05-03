738788786f696102936a95961e4bd9d6f35655b8
andydangit <andy.dang.it@gmail.com>
2022-05-03 15:37:23 -0500

    Feat: Add Google map React value

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
-import GoogleMapReact from 'google-map-react';
-import {Paper, Typography, useMediaQuery } from '@material-ui/core'
-import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined'
-import Rating from '@material-ui/lab';
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOutlinedIcon from "@material-ui/icons/LocationOutlined";
+import Rating from "@material-ui/lab";
 
-import useStyle from './styles'
+import useStyles from "./styles";
 
 function Map() {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
+
+  // fake coordinates until get key from google
+  const coordinates = { lat: 0, lng: 0 };
+
   return (
-    <div>Map</div>
-  )
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }} //Need google map key 
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
 }
 
-export default Map
+export default Map;
