9a20b95884c9775e337c15679c01ac2dff59f9ba
andydangit <andy.dang.it@gmail.com>
2022-05-04 14:42:56 -0500

    Feat: Add temp api key and fix spelling

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-import LocationOutlinedIcon from "@material-ui/icons/LocationOutlined";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import Rating from "@material-ui/lab";
 
 import useStyles from "./styles";
@@ -16,7 +16,7 @@ function Map() {
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }} //Need google map key 
+        bootstrapURLKeys={{ key: "AIzaSyBZoUgXQpRjXFCARqbO0URkcf47z2yID_w" }} //Need google map key 
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={14}
