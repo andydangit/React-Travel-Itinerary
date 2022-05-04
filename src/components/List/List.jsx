2a0726865cd64693b75bf7c50b73e3b6e1df432d
andydangit <andy.dang.it@gmail.com>
2022-05-04 16:16:37 -0500

    Feat: adding menu type

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select} from '@material-ui/core'
 
 import useStyles from './styles'
 
 function List() {
+  const classes = useStyles();
+  const [type, setType] = useState('restaurants');
+
   return (
-    <div>List</div>
+    <div className = {classes.container}> 
+    <Typography variant = "h4"> Restaurants, Hotels & Attractions  around you </Typography>
+    <FormControl className={'classes.formControl'}>
+      <InputLabel> Type </InputLabel>
+      <Select value={type} onChange={(e) => setType (e.target.value)}>
+        <MenuItem value='restaurants'> Restaurants </MenuItem>  
+        <MenuItem value='hotels'> Hotels </MenuItem>  
+        <MenuItem value='attractions'> Attractions </MenuItem>  
+        </Select>
+    </FormControl>
+
+    </div>
   )
 }
 
